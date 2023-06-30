fe6eb33c6d115cdc85efad4fd60e982088924b09
Sascha Wernars <s@scha.nl>
2023-06-30 08:58:21 +0200

    Er kan nu gezocht worden naar een Item

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Header from './Header';
+import SearchItem from './SearchItem'
 import AddItem from './AddItem';
 import Content from './Content';
 import Footer from './Footer';
@@ -9,6 +10,7 @@ const [items, setItems] = useState(JSON.parse(localStorage.getItem
   ('shoppingList')));
 
 const [newItem, setNewItem] = useState('');
+const [search, setSearch] = useState('');
 
 const setAndSaveItems = (newItems) => {
   setItems(newItems);
@@ -48,8 +50,13 @@ const handleSubmit = (e) => {
         setNewItem={setNewItem}
         handleSubmit={handleSubmit}
       />
+      <SearchItem
+        search={search}
+        setSearch={setSearch}
+      />
       <Content 
-        items={items}
+        items={items.filter(item => ((item.item).toLowerCase()).includes
+          (search.toLowerCase()))}
         handleCheck={handleCheck}
         handleDelete={handleDelete}
       />
